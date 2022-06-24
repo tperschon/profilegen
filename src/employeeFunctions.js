800e43d657062d1f494a65140ee649c2c454a571
TPerschon <timperschon@gmail.com>
2022-06-23 23:02:05 -0600

    async/await incorporated

--- src/employeeFunctions.js
+++ src/employeeFunctions.js
@@ -68,15 +68,13 @@ function addEmployees() {
                 name: 'continue'
             }
         ])
-        .then(res => {
-            // if the user wants to add an employee, do so, otherwise create the files
-            res.continue ? addAnEmployee() : createFiles(employees, project);
-        });
+        // if the user wants to add an employee, do so, otherwise create the files
+        .then(res => res.continue ? addAnEmployee() : createFiles(employees, project));
 };
 
 // ask what kind of employee and then ask generic questions about the employee
-function addAnEmployee() {
-    inquirer.prompt([
+async function addAnEmployee() {
+    const inq = await inquirer.prompt([
         {
             type: 'list',
             message: 'What kind of employee?',
@@ -86,59 +84,50 @@ function addAnEmployee() {
         ...employeeQuestions]
     )
     // with our answers
-    .then(res => {
-        // switch statement to decide what to do depending on employee type
-        switch (res.type) {
-            // if it's an engineer, call function to add engineer, feeding in generic data we've gathered so far
-            case 'Engineer': {
-                addEngineer(res.name, res.id, res.email);
-                break;
-            }
-            // if it's an intern, call function to add intern, feeding in generic data we've gathered so far
-            case 'Intern': {
-                addIntern(res.name, res.id, res.email);
-                break;
-            }
-        };
-    });
+    await pickEmployee(inq);
+    addEmployees();
 };
 
+async function pickEmployee(employee) {
+    switch(employee.type) {
+        // if it's an engineer, call function to add engineer, feeding in generic data we've gathered so far
+        case 'Engineer': {
+            await addEngineer(employee.name, employee.id, employee.email);
+            break;
+        }
+        // if it's an intern, call function to add intern, feeding in generic data we've gathered so far
+        case 'Intern': {
+            await addIntern(employee.name, employee.id, employee.email);
+            break;
+        }
+    }
+}
+
 // with given info, ask for engineer-specific information, then push a new Engineer to our employees array
-function addEngineer(name, id, email) {
+async function addEngineer(name, id, email) {
     // ask for their github
-    inquirer.prompt([
+    const inq = await inquirer.prompt([
         {
             type: 'input',
             message: 'What is their GitHub?',
             name: 'github'
         }
     ])
-    // after we get their github 
-    .then(res => {
-        // push a new Engineer to the employees array
-        employees.push(new Engineer(name, id, email, res.github));
-        // we're at the end of the chain, start it over
-        addEmployees();
-    });
+    employees.push(new Engineer(name, id, email, inq.github));
 };
 
 // with given info, ask for intern-specific information, then push a new Intern to our employees array
-function addIntern(name, id, email) {
+async function addIntern(name, id, email) {
     // ask for their school
-    inquirer.prompt([
+    const inq = await inquirer.prompt([
         {
             type: 'input',
             message: 'What school did they attend?',
             name: 'school'
         }
     ])
-    // after we get their school
-    .then(res => {
-        // push a new Intern to the employees array
-        employees.push(new Intern(name, id, email, res.school));
-        // we're at the end of the chain, start it over
-        addEmployees();
-    });
+    // push a new Intern to the employees array
+    employees.push(new Intern(name, id, email, inq.school));
 };
 
 module.exports = createProjectTeam;
