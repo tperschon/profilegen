e77a9bcbe18b422ffc22ff0a88c771f955c622ec
TPerschon <timperschon@gmail.com>
2022-06-27 00:39:32 -0600

    generateStylesheet comment, deleted a test that was too specific

--- tests/generateStylesheet.test.js
+++ tests/generateStylesheet.test.js
@@ -1,15 +1,11 @@
 const generateStylesheet = require('../src/generateStylesheet');
-
+// tests for generateStylesheet
 describe('generateStylesheet', () => {
+    // test if it returns a string
     it('it should return a string', () => {
+        // create a constant with the value assigned by the function
         const sheet = generateStylesheet();
-
+        // ensure that the constant's value is a string
         expect(typeof sheet).toEqual('string');
-    });
-
-    it('string length should be 1481 characters', () => {
-        const sheet = generateStylesheet();
-        
-        expect(sheet.length).toEqual(1481);
     });
 });
